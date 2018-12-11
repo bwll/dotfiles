19d1318fb17623b4b4c510abe2edbf42c3c0d4ae
bwll <bwellington3@gmail.com>
2018-12-10 22:15:15 -0800

    Initial commit

---
+++ brew.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+echo $'installing wget'
+brew install wget
+
+echo $'installing tree'
+brew install tree
+
+echo $'installing yarn'
+brew install yarn
+
+echo $'installing sqlite'
+brew install sqlite
